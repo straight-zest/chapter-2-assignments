c6f55774ef2d8c0b6978be9ccd8fc10371089e60
gabriel.carlile <gabriel.carlile@CLC-LAB209-01.TCSD.tooelesd.org>
2018-09-18 14:17:32 -0600

    first push chapter 2 assignment

---
+++ src/week2_assignments/Week2_assignment1.java
@@ -0,0 +1,25 @@
+package week2_assignments;
+/*
+ * Gabriel Carlile
+ * 9/18
+ */
+public class Week2_assignment1 {
+
+	public static void main(String[] args) {
+	System.out.println("Welcome to Java! Welcome to Computer Science! Worry not little one, programming is fun!");
+	System.out.println("Welcome to Java\r\n" +
+	"Welcome to java\r\n" +
+	"Welcome to java\r\n" +
+	"Welcome to java\r\n" +
+	"Welcome to java\r\n" );
+	System.out.println("'we all die, you either kill yourself or get killed. watcha goin do. watcha goin do'\r\n" +
+	"-Unknown");
+	System.out.println("a    a^2    a^3\r\n" +
+	"1     1      1\r\n" + 
+	"2     4      8\r\n" +
+	"3     9     27\r\n" +
+	"4    16     64");
+
+	}
+
+}
